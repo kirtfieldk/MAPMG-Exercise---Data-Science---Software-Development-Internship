cb489adce06bd034c1e87e82b3c925b41722cb0f
Anthony J Finch <Anthony.J.Finch@kp.org>
2019-10-02 15:16:38 -0400

    Initial commit... with a bonus?

---
+++ BONUS.py
@@ -0,0 +1,16 @@
+def BONUS():
+    """If you've found this file, you get BONUS POINTS.
+    
+    To claim your bonus, include the names of all
+    of Snow White's 7 dwarves in your unit tests.
+    These are:
+    - Grumpy
+    - Happy
+    - Sleepy
+    - Bashful
+    - Sneezy
+    - Dopey
+    - Doc
+    """
+    
+    print("You're on the right track!")
